8dcd39f9b9017607b389daf113a2aa6186e583f2
thomasb81 <thomas.burg@gmail.com>
2014-10-26 15:29:18 +0100

    Fix timing issue in Mixer

--- hdl_src/mixer.v
+++ hdl_src/mixer.v
@@ -8,16 +8,34 @@ module mixer(
     );
 
 
-reg [35:0] mul_result;
+wire [35:0] mul_result;
 reg [21:0] sum2x;
 reg [30:0] calcul;
 reg [17:0] A_q,B_q;
 
+MULT18X18SIO #(
+.AREG(1),
+.BREG(1),
+.PREG(1),
+.B_INPUT("DIRECT")
+) mixer_mul (
+.A(A),
+.B(B),
+.CEA(1'b1),
+.CEB(1'b1),
+.CEP(1'b1),
+.CLK(clk),
+.RSTA(rst),
+.RSTB(rst),
+.RSTP(rst),
+.BCIN(18'h00000),
+.BCOUT(),
+.P(mul_result)
+);
 
 always @(posedge clk) begin
   A_q <= A;
   B_q <= B;
-  mul_result <= A_q * B_q;
   sum2x <= (A_q<<1) + (B_q<<1);
   calcul = sum2x - mul_result[35:17] - (1<<18);
 end
